1dac0861c46f9cb1e7a167f43c711fc20e230c58
Holman <84192726+soyholman@users.noreply.github.com>
2021-12-03 07:51:35 -0600

    Snow efect

--- js/encuesta.js
+++ js/encuesta.js
@@ -6,7 +6,7 @@ $(function (){
     var result;
     var countStart = 21;
     var timer;
-    
+
     $(".startGame").on("click", function () {  
         startGame();
         $("#playAgain").hide();
@@ -24,6 +24,9 @@ $(function (){
         playGame();
     
         setTimeout (function (){
+
+            var can=document.querySelector('#quizArea');
+            can.width=W;can.height=H;can.style.background='white';
             $("#quizArea").show();
         }, 1000);
     }
@@ -79,3 +82,46 @@ $(function (){
     }
         
     })
+
+
+    "use strict"
+class Circle {
+constructor(x,y,r,c,v,e){
+this.x=x;
+this.y=y;
+this.r=r;
+this.c=c;
+this.v=v;
+this.e=e;
+this.a=.5;
+}
+draw(){this.sn+=.3
+       this.y-=this.v;
+       this.y<this.e?this.a-=.02:'';
+       san.beginPath();
+       san.arc(this.x,this.y,this.r,0,44/7,false);
+       san.fillStyle=`rgb(0,0,255)`;
+       san.fill()
+       san.closePath();
+}
+static push(n){var rad=Mnx(1,5),cx=Mnx(rad,W-rad),cy=n?Mnx(H+rad,H+rad+100):Mnx(H*1.2,H*.3),cl=Mnx(100,250),vl=Mnx(.5,2),en=Mnx(H*.8,H*.25);
+return new Circle(cx,cy,rad,cl,vl,en)}
+}
+var Mnx=(n,x)=>{return Math.random()*(x-n)+n},W=window.innerWidth,H=window.innerHeight,san;
+var ar=[];
+for(var i=0;i<550;i++){
+ar.push(Circle.push(false))
+}
+
+var draw=()=>{for(var i=0;i<ar.length;i++){
+ar[i].draw();if(ar[i].a<0){ar[i]=Circle.push(true)};}}
+var init=()=>{
+var can=document.querySelector('canvas');
+can.width=W;can.height=H;can.style.background='white';can.style.position='fixed';can.style.left=0;can.style.top=0;window.backdropFilter='blur(2px)';san=can.getContext('2d');
+var up=()=>{
+//san.fillStyle=`rgba(0,0,0,.1)`
+san.clearRect(0,0,W,H);
+draw();webkitRequestAnimationFrame(up);};
+webkitRequestAnimationFrame(up);
+};
+onload=init;
