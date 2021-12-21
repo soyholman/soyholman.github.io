d7ff1220e882ff691bca200efdb4020796b0f88a
Holman <84192726+soyholman@users.noreply.github.com>
2021-12-20 22:04:43 -0600

    Nueva trivia de cultura general

---
+++ js/trivia_cultura_generalf1.js
@@ -0,0 +1,287 @@
+var QUESTIONS = [
+    {
+        question: "¿Cuál es el álbum más vendido de la historia??",
+        answers: ["The Dark Side of the Moon de Pink Floyd", "Thriller de Michael Jackson", "Of The Wall de Michael Jackson","el ultimo tour del mundo de Bad Bunny"],
+        correctAnswer: "Thriller de Michael Jackson",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/72010eab-751b-4d43-a8ae-1ae79305bbcc.jpg"
+    },
+    {
+        question: "¿En qué año inició la Segunda Guerra Mundial?",
+        answers: ["1940", "1938", "1920", "1939"],
+        correctAnswer: "1939",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/631796d3-55f4-4c24-987e-4bd25f5cbb99.jpg"
+    },
+    {
+     
+        question: "¿Con qué color se representa la esperanza?",
+        answers: [ "Amarillo", "Verde", "Blanco","Negro"],
+        correctAnswer: "Verde",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/5887370e-3997-413e-869b-d9b70e991fb0.jpg"
+    },
+    {
+		question: "¿A qué estilo arquitectónico pertenece la Catedral de Notre Dame en París?",
+        answers: ["Neoclásico", "Gótico", "Barroco","Moderno"],
+        correctAnswer: "Gótico",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/35dbee2a-59d3-4f3f-9f6f-c86471dc712f.jpg"
+    },
+    {	
+		question: "¿Qué era el Concorde?",
+        answers: ["Un pan ", "Un Tipo de vino", "Un buque petrolero","Un avión Supersónico"],
+        correctAnswer: "Un avión Supersónico",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/b8ea3f41-f73d-4420-92b6-5055f180c2b5.jpg"
+    },
+    {
+		question: "¿Cuál es la capital de Croacia?",
+        answers: ["Sofía", "Viena", "Ulán Bator","Zagreb"],
+        correctAnswer: "Zagreb",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/c468ce7e-e9f9-499b-8385-c110478af98c.jpg"
+    },
+    {
+		question: "¿Contra qué país se usó la primera bomba atómica del mundo?",
+        answers: ["Unión Soviética", "Japón", "Estados Unidos","Alemania"],
+        correctAnswer: "Japón",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/1dec8bc6-ef30-4e64-a773-ee6239a292b1.jpg"
+    },
+    {
+		question: "¿Cuál es el animal más rápido del mundo?",
+        answers: ["Guepardo", "Delfin", "Pez Vela","Halcón peregrino"],
+        correctAnswer: "Halcón peregrino",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/372b0d30-148a-44c8-9e82-09a523f44d78.jpg"
+    },
+    {
+		question: "¿Cuántos corazones tiene un pulpo?",
+        answers: ["5", "4", "3","No tiene"],
+        correctAnswer: "3",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/18fbb9d9-fe9c-490b-8d27-182dc7f55b04.jpg"
+    },
+    {
+		question: "¿Qué océano es el más grande por su extensión?",
+        answers: ["Oceano índico", "Oceano Pacífico", "Oceano Atlántico","Oceano Ártico"],
+        correctAnswer: "Oceano Pacífico",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/69013c49-60aa-4d67-a05d-4f2d0fa98758/0cc29a4e-d215-4e44-b6a0-b7348e9135fc.jpg"
+    }
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 12;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+
+    // Function gets Questions-Data and append it to HTML and
+    // returns element containing correct answer
+    function getTrivia() {
+
+        var correctAnswer;
+        $("#answers").empty();
+        $("#question-header").text(QUESTIONS[questionIndex].question);
+        QUESTIONS[questionIndex].answers.forEach(function (answer) {
+            var div = $("<div>").append(answer);
+            div.appendTo($("#answers"));
+            if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+                correctAnswer = div;
+            }
+            div.addClass("hover"); // Adds hover effect to each answer element
+        });
+
+        return correctAnswer;
+    }
+
+    // Function adds click event to each answer element and
+    // checks for correct answer. If clicked element has
+    // correct answer, text turns green and appends check mark
+    // else text turns color gray and appends an X mark
+    function checkCorrectAnswer() {
+
+        $("#answers").children().click(function () {
+
+            resetIntervalTimer();
+
+            if ($(this).text() === correctAnswerElement.text()) {
+
+                correct++;
+                showCorrectAnswer();
+
+            } else {
+
+                incorrect++;
+                $(this).addClass("wrong");
+                $(this).append("<span> &#10008;</span>");
+                $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+                correctAnswerElement.append("<span> &#10004;</span>");
+                correctAnswerElement.addClass("correct");
+
+                $("#message").css("display", "block");
+                $("#timeLeft").css("display", "none");
+                $("#message").text("INCORRECTO");
+
+                $("#imageTrivia").attr("src", "https://images.angelpub.com/2016/35/39703/wrong.gif");
+
+            }
+
+            $(this).parent().children().off("click"); // Prevent click event
+            $("#answers").children().removeClass("hover"); // Turn off hover effect
+
+        });
+
+    }
+
+    // Function shows the correct trivia answer
+    function showCorrectAnswer() {
+
+        $("#answers").children().not(correctAnswerElement).addClass("wrong");
+        correctAnswerElement.append("<span> &#10004;</span>");
+        correctAnswerElement.addClass("correct");
+
+        $("#answers").children().removeClass("hover");
+        $("#answers").children().off("click");
+
+        $("#message").css("display", "block");
+        $("#timeLeft").css("display", "none");
+        $("#message").text("CORRECTO");
+
+        $("#imageTrivia").attr("src", "https://media1.tenor.com/images/c69fe60c4e179c1cf21726c224353de8/tenor.gif?itemid=11296495");
+
+    }
+
+    // Function creates a countdown which is display on HTML
+    // If timeLeft variable reach zero, correct answer shows up
+    // and then wait for 4 seconds to restart
+    function intervalTimer() {
+
+        timer = setInterval(function () {
+
+            timeLeft--;
+            $("#timerSeconds").text(timeLeft);
+
+            if (timeLeft == 0) {
+                incorrect++;
+                showCorrectAnswer();
+                clearInterval(timer);
+                timeLeft = 15;
+                $("#timeLeft").css("display", "none");
+                $("#message").text("se acabó el tiempo!");
+                $("#message").css("display", "block");
+
+                setTimeout(function () {
+                    questionIndex++;
+					startGame();
+                    $("#message").css("display", "none");
+                    $("#timeLeft").css("display", "block");
+                    $("#timerSeconds").text(timeLeft);
+                    $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+                  
+                }, 5000);
+            }
+
+        }, 1000);
+    }
+
+
+    // Function clears countdown after an answer is click and
+    // waits four seconds to restart
+    function resetIntervalTimer() {
+
+        clearInterval(timer);
+
+        setTimeout(function () {
+            questionIndex++;
+            timeLeft = 15;
+			startGame();
+            $("#message").css("display", "none");
+            $("#message").text("SE HA TERMINADO EL TIEMPO!");
+            $("#timeLeft").css("display", "block");
+            $("#timerSeconds").text(timeLeft);
+            $("#imageTrivia").attr("src",QUESTIONS[questionIndex].image);
+        
+
+        }, 2000);
+
+    }
+
+    // Function starts the game
+    // If questionIndex reach QUESTIONS length, game ends and score
+    // shows up, otherwise the games keeps running until last question
+    function startGame() {
+
+        if (questionIndex <QUESTIONS.length) {
+
+            correctAnswerElement = getTrivia();
+
+            checkCorrectAnswer();
+
+            intervalTimer();
+
+        } else {
+            questionIndex = 0;
+            $("#card").css("display", "none");
+            $("#correct").text("Correcto: " + correct);
+            $("#incorrect").text("Incorrecto: " + incorrect);
+            $("#score").css("display", "block");
+        }
+
+    }
+
+    // Click event button starts game
+    $("#button").click(function(){
+        correct = 0;
+        incorrect = 0;
+        questionIndex = 0;
+        $("#card").css("display", "flex");
+        $("#button").css("display", "none");
+        startGame();
+    });
+
+    // Click event close score pop-up
+    $("#close").click(function(){
+        $("#score").css("display", "none");
+        $("#button").css("display", "inline-block");
+    });
+
+});
+
+    "use strict"
+class Circle {
+constructor(x,y,r,c,v,e){
+this.x=x;
+this.y=y;
+this.r=r;
+this.c=c;
+this.v=v;
+this.e=e;
+this.a=.2;
+}
+draw(){this.sn+=.3
+       this.y-=this.v;
+       this.y<this.e?this.a-=.02:'';
+       san.beginPath();
+       san.arc(this.x,this.y,this.r,0,44/7,false);
+       san.fillStyle=`rgb(0,0,240)`;
+       san.fill()
+       san.closePath();
+}
+static push(n){var rad=Mnx(1,5),cx=Mnx(rad,W-rad),cy=n?Mnx(H+rad,H+rad+100):Mnx(H*1.2,H*.3),cl=Mnx(100,250),vl=Mnx(.5,2),en=Mnx(H*.8,H*.25);
+return new Circle(cx,cy,rad,cl,vl,en)}
+}
+var Mnx=(n,x)=>{return Math.random()*(x-n)+n},W=window.innerWidth,H=window.innerHeight,san;
+var ar=[];
+for(var i=0;i<550;i++){
+ar.push(Circle.push(false))
+}
+
+var draw=()=>{for(var i=0;i<ar.length;i++){
+ar[i].draw();if(ar[i].a<0){ar[i]=Circle.push(true)};}}
+var init=()=>{
+var can=document.querySelector('canvas');
+can.width=W;can.height=H;can.style.background='white';can.style.position='fixed';can.style.left=0;can.style.top=0;window.backdropFilter='blur(2px)';san=can.getContext('2d');
+var up=()=>{
+//san.fillStyle=`rgba(0,0,0,.1)`
+san.clearRect(0,0,W,H);
+draw();webkitRequestAnimationFrame(up);};
+webkitRequestAnimationFrame(up);
+};
+onload=init;
