6165c64592b52796fa18101d55cd9ae1d996987e
Holman <84192726+soyholman@users.noreply.github.com>
2021-12-29 09:29:39 -0600

    mejoras practicas

--- js/trivia_lugar_seguro.js
+++ js/trivia_lugar_seguro.js
@@ -67,6 +67,7 @@ $(function () {
             if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
                 correctAnswer = div;
             }
+            $("#Titles").css("font-size", "medium");
             div.addClass("hover"); // Adds hover effect to each answer element
         });
 
