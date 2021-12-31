5cff5d1b6c5716fcac010ba488cfe28ff9a689f4
Holman <84192726+soyholman@users.noreply.github.com>
2021-12-31 11:28:30 -0600

    Buscador de trivias

---
+++ js/trivia_filter.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+ 
+    li = document.getElementsByClassName("box");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
