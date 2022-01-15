60d6b969fb9b0cf7ebeb79a9db31e1d1a743a7b9
Holman <84192726+soyholman@users.noreply.github.com>
2022-01-15 10:22:11 -0600

    Mejoras de recomendaciones y validacion de usuarios

--- js/firebase/recomendaciones.js
+++ js/firebase/recomendaciones.js
@@ -3,18 +3,20 @@ var user = firebase.auth().currentUser;
 auth.onAuthStateChanged(user=>{
 
 const nombre=document.getElementById('getname');
+var hoy = new Date();
+var dd = hoy.getDate();
+var mm = hoy.getMonth()+1;
+var yyyy = hoy.getFullYear();
+var data_carpet=dd+'_'+mm+'_'+yyyy;
 
-var firebaseref=firebase.database().ref('Sugerencias/'+user.displayName);
+var firebaseref=firebase.database().ref('Sugerencias/'+user.displayName+"/"+ data_carpet.toString());
 const fs=firebase.firestore();
 
 document.querySelector("#Enviar").addEventListener("click",()=>{
 const dinamica= document.getElementById('dinamic');
 const sugerencia=document.getElementById('suggest').value;
 
-var hoy = new Date();
-var dd = hoy.getDate();
-var mm = hoy.getMonth()+1;
-var yyyy = hoy.getFullYear();
+
 
 if(dinamica=="" || sugerencia==""){
 
@@ -45,7 +47,8 @@ Swal.fire({
         firebaseref.push({
 fecha:dd+'/'+mm+'/'+yyyy,
 dinamica:dinamica.value,
-usuario:user.email,
+usuario:user.displayName,
+correo:user.email,
       tema:sugerencia
 });
 document.getElementById('suggest').value = ''
