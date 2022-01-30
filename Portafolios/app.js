2b400e5364f6d29a8b8a55b029a4b22645a8440e
Holman Morales <hmoralesguido@gmail.com>
2022-01-30 17:14:07 -0600

    Anexo de mi portafolio

---
+++ Portafolios/app.js
@@ -0,0 +1,43 @@
+
+
+
+var tag, nav;
+
+var navAnimStarted = false;
+
+onload = function(){
+
+    
+    
+    tag = document.getElementById("tag");
+    nav = document.getElementById("name");
+    body = document.getElementById("body");
+    
+    AOS.init();
+    
+    
+    
+        body.style.overflowY = "auto";
+        body.style.overflowX = "hidden"
+        nav.style.transition = "0.6s";
+        nav.style.height = "10vh";
+        
+        
+        
+        
+    
+    
+    
+    
+    // setTimeout(changeTag, 1000)
+    
+    // setInterval(function(){
+    //     changeTag()
+        
+    // }, 4000)
+
+}
+
+
+
+
