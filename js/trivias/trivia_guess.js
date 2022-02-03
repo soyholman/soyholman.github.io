5e317d4cec50d802fd1cea3727d700f613623f58
Holman Morales <hmoralesguido@gmail.com>
2022-02-03 13:10:56 -0600

    Nueva trivia guess

---
+++ js/trivias/trivia_guess.js
@@ -0,0 +1,351 @@
+var QUESTIONS = [
+    {
+        question: "¿Quién es esta melancólica mujer?",
+        answers: ["Reina Isabel I","Teresa de la Parra","Agatha Christie","Margaret Thatcher"],
+        correctAnswer: "Agatha Christie",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/ff83f1fa-f2f3-45f3-96a1-4cc75017b9b0.jpg"
+    },
+    {
+        question: "¿Quién es esta mujer?",
+        answers: ["Isabel I de Castilla","Juana de Arco","Lyudmila Pavlichenko","La Virgen María"],
+        correctAnswer: "Juana de Arco",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/5ea3d9b3-45b1-48c6-8c5a-ecd0b3419b24.jpg"
+    },
+    {
+        question: "¿Quién es este hombre?",
+        answers: ["Michelangelo","Rasputín","Calígula","Charles Darwin"],
+        correctAnswer: "Rasputín",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/082ef876-a805-4d06-ae60-3a25f6050946.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es el hombre que sostiene al gato?",
+        answers: ["Pablo Picasso","Gustav Klimt","Francisco Pizarro","Antonio Vivaldi"],
+        correctAnswer: "Gustav Klimt",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/7202c271-bbfe-463e-aa47-6579f18c4a77.jpg"
+    }
+    ,
+    {
+        question: "¿A quién representa el busto de esta misteriosa mujer?",
+        answers: ["Nefertiti","Venus","Cleopatra","Hipatía"],
+        correctAnswer: "Nefertiti",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/31b25e5f-2829-447a-94c4-121545e068e9.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es este hombre?",
+        answers: ["Moon Jae-in","Mao Tse Tung","Kim Jong Un","Xi Jinping"],
+        correctAnswer: "Kim Jong Un",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/6b908d58-e1fd-4cba-84c0-e1878d0622a3.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es este hombre?",
+        answers: ["Antonio José de Sucre","Simón Bolívar","Francisco de Miranda","José Francisco de San Martín"],
+        correctAnswer: "Simón Bolívar",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/fb1c663f-06a0-4b2c-a89e-6eef23d020be.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es el dueño del bigote?",
+        answers: ["Charles Darwin","Friedrich Nietzsche","Isaac Newton","Edgar Allan Poe"],
+        correctAnswer: "Friedrich Nietzsche",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/e3fa2061-1703-4625-8f59-d4583c37b356.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es este hombre? -Una pista, es músico-",
+        answers: ["John Sebastian Bach","Wolfgang Amadeus Mozart","Fryderyk Franciszek Chopin","Ludwig Van Beethoven"],
+        correctAnswer: "Ludwig Van Beethoven",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/d63e6855-e7f2-4003-8879-42487f5a1936.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es esta elegante mujer?",
+        answers: ["Maria Antonieta","Leonor de Aquitania","Juana de Arco","Émilie du Châtelet"],
+        correctAnswer: "Maria Antonieta",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/e9068954-2b32-49b8-9fac-ada97fa46dab.jpg"
+    }
+    ,
+    {
+        question: "¿Quién es esta mujer?",
+        answers: ["Marie Curie","Jocelyn Bell","Emmy Noerther","Rosalind Franklin"],
+        correctAnswer: "Marie Curie",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/e2fc5b70-7a48-4c0a-ae47-80069eba05a0.jpg"
+    },
+    {
+        question: "¿Quién es este hombre?",
+        answers: ["Miguel de Cervantes","Francisco de Quevedo","William Shakespeare","Aristóteles"],
+        correctAnswer: "Miguel de Cervantes",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/1da74e2e-dfd5-4cb2-8265-02edf554c2a7/95bf5393-5af1-413a-b070-3294b1188e76.jpg"
+    }
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 15;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+
+    // Function gets Questions-Data and append it to HTML and
+    // returns element containing correct answer
+    function getTrivia() {
+
+        var correctAnswer;
+        $("#answers").empty();
+        $("#question-header").text(QUESTIONS[questionIndex].question);
+        QUESTIONS[questionIndex].answers.forEach(function (answer) {
+            var div = $("<div>").append(answer);
+            div.appendTo($("#answers"));
+            if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+                correctAnswer = div;
+            }
+            div.addClass("hover");
+            $("#Titles").css("font-size", "medium"); // Adds hover effect to each answer element
+        });
+
+        return correctAnswer;
+    }
+
+    // Function adds click event to each answer element and
+    // checks for correct answer. If clicked element has
+    // correct answer, text turns green and appends check mark
+    // else text turns color gray and appends an X mark
+    function checkCorrectAnswer() {
+
+        $("#answers").children().click(function () {
+
+            resetIntervalTimer();
+
+            if ($(this).text() === correctAnswerElement.text()) {
+
+                correct++;
+                showCorrectAnswer();
+
+            } else {
+
+                incorrect++;
+                $(this).addClass("wrong");
+                $(this).append("<span> &#10008;</span>");
+                $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+                correctAnswerElement.append("<span> &#10004;</span>");
+                correctAnswerElement.addClass("correct");
+
+                $("#message").css("display", "block");
+                $("#timeLeft").css("display", "none");
+                $("#message").text("INCORRECTO");
+
+                $("#imageTrivia").attr("src", "https://media.baamboozle.com/uploads/images/49869/1618223208_15917_gif-url.gif");
+
+            }
+
+            $(this).parent().children().off("click"); // Prevent click event
+            $("#answers").children().removeClass("hover"); // Turn off hover effect
+
+        });
+
+    }
+
+    // Function shows the correct trivia answer
+    function showCorrectAnswer() {
+
+        $("#answers").children().not(correctAnswerElement).addClass("wrong");
+        correctAnswerElement.append("<span> &#10004;</span>");
+        correctAnswerElement.addClass("correct");
+
+        $("#answers").children().removeClass("hover");
+        $("#answers").children().off("click");
+        $("#timer").css("display", "none");
+        $("#message").css("display", "none");
+        $("#timeLeft").css("display", "none");
+        // $("#message").text("CORRECTO");
+
+        Swal.fire({
+           
+            icon: 'success',
+            title: 'Correcto',
+            showConfirmButton: false,
+            timer: 1600
+          })
+    }
+    function showCorrectAnswertimer() {
+
+        $("#answers").children().not(correctAnswerElement).addClass("wrong");
+        correctAnswerElement.append("<span> &#10004;</span>");
+        correctAnswerElement.addClass("correct");
+
+        $("#answers").children().removeClass("hover");
+        $("#answers").children().off("click");
+      
+        $("#message").css("display", "none");
+        $("#timeLeft").css("display", "none");
+        // $("#message").text("CORRECTO");
+
+    }
+
+    // Function creates a countdown which is display on HTML
+    // If timeLeft variable reach zero, correct answer shows up
+    // and then wait for 4 seconds to restart
+    function intervalTimer() {
+
+        timer = setInterval(function () {
+
+            timeLeft--;
+            $("#timerSeconds").text(timeLeft);
+
+            if (timeLeft == 0) {
+                incorrect++;
+              showCorrectAnswertimer();
+                clearInterval(timer);
+                timeLeft = 15;
+                $("#timeLeft").css("display", "none");
+                $("#message").text("se acabó el tiempo!");
+                $("#message").css("display", "block");
+
+                setTimeout(function () {
+                    questionIndex++;
+					startGame();
+                    $("#message").css("display", "none");
+                    $("#timeLeft").css("display", "block");
+                    $("#timerSeconds").text(timeLeft);
+                    $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+                  
+                }, 5000);
+            }
+
+        }, 1000);
+    }
+
+
+    // Function clears countdown after an answer is click and
+    // waits four seconds to restart
+    function resetIntervalTimer() {
+
+        clearInterval(timer);
+
+        setTimeout(function () {
+            questionIndex++;
+            timeLeft = 15;
+			startGame();
+            $("#message").css("display", "none");
+            $("#timer").css("display", "flex");
+            $("#message").text("SE HA TERMINADO EL TIEMPO!");
+            $("#timeLeft").css("display", "block");
+            $("#timerSeconds").text(timeLeft);
+            $("#imageTrivia").attr("src",QUESTIONS[questionIndex].image);
+        
+
+        }, 2000);
+
+    }
+
+    // Function starts the game
+    // If questionIndex reach QUESTIONS length, game ends and score
+    // shows up, otherwise the games keeps running until last question
+    function startGame() {
+
+        if (questionIndex <QUESTIONS.length) {
+
+            correctAnswerElement = getTrivia();
+
+            checkCorrectAnswer();
+
+            intervalTimer();
+
+        } else {
+            questionIndex = 0;
+            $("#card").css("display", "none");
+            $("#correct").text("Correcto: " + correct);
+            $("#incorrect").text("Incorrecto: " + incorrect);
+            $("#score").css("display", "block");
+            var user = firebase.auth().currentUser;
+auth.onAuthStateChanged(user=>{
+    var hoy = new Date();
+var dd = hoy.getDate();
+var mm = hoy.getMonth()+1;
+var yyyy = hoy.getFullYear();
+var data_carpet=dd+'_'+mm+'_'+yyyy;
+
+var firebaseref=firebase.database().ref('Trivia/'+document.getElementById('Titles').textContent+'/'+user.displayName);
+const fs=firebase.firestore();
+
+
+        firebaseref.push({
+fecha:dd+'/'+mm+'/'+yyyy,
+usuario:user.displayName,
+Correctas:correct,
+incorrectas:incorrect
+});
+   
+
+
+})
+      
+        }
+
+    }
+
+    // Click event button starts game
+    $("#button").click(function(){
+        correct = 0;
+        incorrect = 0;
+        questionIndex = 0;
+        $("#card").css("display", "flex");
+        $("#button").css("display", "none");
+        startGame();
+    });
+
+    // Click event close score pop-up
+    $("#close").click(function(){
+        $("#score").css("display", "none");
+        $("#button").css("display", "inline-block");
+     
+    });
+
+});
+
+    "use strict"
+class Circle {
+constructor(x,y,r,c,v,e){
+this.x=x;
+this.y=y;
+this.r=r;
+this.c=c;
+this.v=v;
+this.e=e;
+this.a=.2;
+}
+draw(){this.sn+=.3
+       this.y-=this.v;
+       this.y<this.e?this.a-=.02:'';
+       san.beginPath();
+       san.arc(this.x,this.y,this.r,0,44/7,false);
+       san.fillStyle=`rgb(0,0,240)`;
+       san.fill()
+       san.closePath();
+}
+static push(n){var rad=Mnx(1,5),cx=Mnx(rad,W-rad),cy=n?Mnx(H+rad,H+rad+100):Mnx(H*1.2,H*.3),cl=Mnx(100,250),vl=Mnx(.5,2),en=Mnx(H*.8,H*.25);
+return new Circle(cx,cy,rad,cl,vl,en)}
+}
+var Mnx=(n,x)=>{return Math.random()*(x-n)+n},W=window.innerWidth,H=window.innerHeight,san;
+var ar=[];
+for(var i=0;i<550;i++){
+ar.push(Circle.push(false))
+}
+
+var draw=()=>{for(var i=0;i<ar.length;i++){
+ar[i].draw();if(ar[i].a<0){ar[i]=Circle.push(true)};}}
+var init=()=>{
+var can=document.querySelector('canvas');
+can.width=W;can.height=H;can.style.background='white';can.style.position='fixed';can.style.left=0;can.style.top=0;window.backdropFilter='blur(2px)';san=can.getContext('2d');
+var up=()=>{
+//san.fillStyle=`rgba(0,0,0,.1)`
+san.clearRect(0,0,W,H);
+draw();webkitRequestAnimationFrame(up);};
+webkitRequestAnimationFrame(up);
+};
+onload=init;
