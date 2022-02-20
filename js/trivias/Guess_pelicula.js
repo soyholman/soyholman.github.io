9f0a4f4eb0676abcf04c6601312d317b4ccb2d5e
Holman Morales <hmoralesguido@gmail.com>
2022-02-20 13:34:31 -0600

    Nueva trivia de pelicula y efecto zoom en boton de novedades

---
+++ js/trivias/Guess_pelicula.js
@@ -0,0 +1,367 @@
+var QUESTIONS = [
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Rocky", "Depredador","Rambo"],
+        correctAnswer: "Rambo",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/d7a556fc-4a4f-44e6-9514-2f1fccd55a5b.jpg"
+    },
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Alien en el octavo pasajero","Interstellar","Gravity"],
+        correctAnswer: "Gravity",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/4ff4c8b4-2bbc-42f0-964c-d422709322c7.jpg"
+    },
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["El Show de Truman","¡Sí señor!","Todo poderoso"],
+        correctAnswer: "El Show de Truman",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/d78c2048-9278-44fb-8b4f-e064c219bcab.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Pulp Fiction","Duro de matar","12 monos"],
+        correctAnswer: "Pulp Fiction",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/d2d0eeeb-d4c1-4f4d-8a6d-2aebfcb63027.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Forrest Gump","Salvando al soldado Ryan",
+"Depredador"],
+        correctAnswer: "Forrest Gump",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/9643603c-79e2-4066-9688-f454d7f2bc93.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Scary Movie 4","Scary Movie 5","Scary Movie 3"],
+        correctAnswer: "Scary Movie 3",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/bf4263f9-5a8d-4378-a278-fc593c41fffd.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Rápidos y Furiosos 6","Rápidos y Furiosos 4","Rápidos y Furiosos 2"],
+        correctAnswer: "Rápidos y Furiosos 6",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/fcfa1803-0513-4cb5-94b9-6aebcfa50c66.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Chicago","El gran Gatsby","La era del rock"],
+        correctAnswer: "Chicago",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/eac58f2f-31ec-416b-a3af-8a647cdd4ea8.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Juntos y revueltos","Como si fuera la primera vez","La mejor de mis bodas"],
+        correctAnswer: "Como si fuera la primera vez",
+        image: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/b7261d17-71d4-4834-8e9d-3258bbd7192c/7b7bb292-b01e-4b6d-815b-9330e0f4c578.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Sensatez y sentimientos","Titanic","Hamlet","Romeo y Julieta"],
+        correctAnswer: "Titanic",
+        image: "http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2016/11/1.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Patch Adams","Mrs. Doubtfire","Jumanji"],
+        correctAnswer: "Mrs. Doubtfire",
+        image: "http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2016/11/12.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Rescatando al soldado Ryan","Bourne: Identidad desconocida","La supremacía Bourne"],
+        correctAnswer: "Rescatando al soldado Ryan",
+        image: "http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2016/11/16.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Batman Forever","Batman & Robin"],
+        correctAnswer: "Batman Forever",
+        image: "http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2016/11/21.jpg"
+    }
+    ,
+    {
+        question: "¿A qué película pertenece esta escena?",
+        answers: ["Mi pobre angelito 4","Mi pobre angelito 2"],
+        correctAnswer: "Mi pobre angelito 2",
+        image: "http://quizupso.pansyequitysa.netdna-cdn.com/q/wp-content/uploads/2016/11/27.jpg"
+    }
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 15;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+
+    // Function gets Questions-Data and append it to HTML and
+    // returns element containing correct answer
+    function getTrivia() {
+
+        var correctAnswer;
+        $("#answers").empty();
+        $("#question-header").text(QUESTIONS[questionIndex].question);
+        QUESTIONS[questionIndex].answers.forEach(function (answer) {
+            var div = $("<div>").append(answer);
+            div.appendTo($("#answers"));
+            if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+                correctAnswer = div;
+            }
+            div.addClass("hover");
+            $("#Titles").css("font-size", "medium"); // Adds hover effect to each answer element
+        });
+
+        return correctAnswer;
+    }
+
+    // Function adds click event to each answer element and
+    // checks for correct answer. If clicked element has
+    // correct answer, text turns green and appends check mark
+    // else text turns color gray and appends an X mark
+    function checkCorrectAnswer() {
+
+        $("#answers").children().click(function () {
+
+            resetIntervalTimer();
+
+            if ($(this).text() === correctAnswerElement.text()) {
+
+                correct++;
+                showCorrectAnswer();
+
+            } else {
+
+                incorrect++;
+                $(this).addClass("wrong");
+                $(this).append("<span> &#10008;</span>");
+                $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+                correctAnswerElement.append("<span> &#10004;</span>");
+                correctAnswerElement.addClass("correct");
+
+                $("#message").css("display", "block");
+                $("#timeLeft").css("display", "none");
+                $("#message").text("INCORRECTO");
+
+                $("#imageTrivia").attr("src", "https://media.baamboozle.com/uploads/images/49869/1618223208_15917_gif-url.gif");
+
+            }
+
+            $(this).parent().children().off("click"); // Prevent click event
+            $("#answers").children().removeClass("hover"); // Turn off hover effect
+
+        });
+
+    }
+
+    // Function shows the correct trivia answer
+    function showCorrectAnswer() {
+
+        $("#answers").children().not(correctAnswerElement).addClass("wrong");
+        correctAnswerElement.append("<span> &#10004;</span>");
+        correctAnswerElement.addClass("correct");
+
+        $("#answers").children().removeClass("hover");
+        $("#answers").children().off("click");
+        $("#timer").css("display", "none");
+        $("#message").css("display", "none");
+        $("#timeLeft").css("display", "none");
+        // $("#message").text("CORRECTO");
+
+        Swal.fire({
+           
+            icon: 'success',
+            title: 'Correcto',
+            showConfirmButton: false,
+            timer: 1600
+          })
+    }
+    function showCorrectAnswertimer() {
+
+        $("#answers").children().not(correctAnswerElement).addClass("wrong");
+        correctAnswerElement.append("<span> &#10004;</span>");
+        correctAnswerElement.addClass("correct");
+
+        $("#answers").children().removeClass("hover");
+        $("#answers").children().off("click");
+      
+        $("#message").css("display", "none");
+        $("#timeLeft").css("display", "none");
+        // $("#message").text("CORRECTO");
+
+    }
+
+    // Function creates a countdown which is display on HTML
+    // If timeLeft variable reach zero, correct answer shows up
+    // and then wait for 4 seconds to restart
+    function intervalTimer() {
+
+        timer = setInterval(function () {
+
+            timeLeft--;
+            $("#timerSeconds").text(timeLeft);
+
+            if (timeLeft == 0) {
+                incorrect++;
+              showCorrectAnswertimer();
+                clearInterval(timer);
+                timeLeft = 15;
+                $("#timeLeft").css("display", "none");
+                $("#message").text("se acabó el tiempo!");
+                $("#message").css("display", "block");
+
+                setTimeout(function () {
+                    questionIndex++;
+					startGame();
+                    $("#message").css("display", "none");
+                    $("#timeLeft").css("display", "block");
+                    $("#timerSeconds").text(timeLeft);
+                    $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+                  
+                }, 5000);
+            }
+
+        }, 1000);
+    }
+
+
+    // Function clears countdown after an answer is click and
+    // waits four seconds to restart
+    function resetIntervalTimer() {
+
+        clearInterval(timer);
+
+        setTimeout(function () {
+            questionIndex++;
+            timeLeft = 15;
+			startGame();
+            $("#message").css("display", "none");
+            $("#timer").css("display", "flex");
+            $("#message").text("SE HA TERMINADO EL TIEMPO!");
+            $("#timeLeft").css("display", "block");
+            $("#timerSeconds").text(timeLeft);
+            $("#imageTrivia").attr("src",QUESTIONS[questionIndex].image);
+        
+
+        }, 2000);
+
+    }
+
+    // Function starts the game
+    // If questionIndex reach QUESTIONS length, game ends and score
+    // shows up, otherwise the games keeps running until last question
+    function startGame() {
+
+        if (questionIndex <QUESTIONS.length) {
+
+            correctAnswerElement = getTrivia();
+
+            checkCorrectAnswer();
+
+            intervalTimer();
+
+        } else {
+            questionIndex = 0;
+            $("#card").css("display", "none");
+            $("#correct").text("Correcto: " + correct);
+            $("#incorrect").text("Incorrecto: " + incorrect);
+            $("#score").css("display", "block");
+            var user = firebase.auth().currentUser;
+auth.onAuthStateChanged(user=>{
+    var hoy = new Date();
+var dd = hoy.getDate();
+var mm = hoy.getMonth()+1;
+var yyyy = hoy.getFullYear();
+var data_carpet=dd+'_'+mm+'_'+yyyy;
+
+var firebaseref=firebase.database().ref('Trivia/'+document.getElementById('Titles').textContent+'/'+user.displayName);
+const fs=firebase.firestore();
+
+
+        firebaseref.push({
+fecha:dd+'/'+mm+'/'+yyyy,
+usuario:user.displayName,
+Correctas:correct,
+incorrectas:incorrect
+});
+   
+
+
+})
+      
+        }
+
+    }
+
+    // Click event button starts game
+    $("#button").click(function(){
+        correct = 0;
+        incorrect = 0;
+        questionIndex = 0;
+        $("#card").css("display", "flex");
+        $("#button").css("display", "none");
+        startGame();
+    });
+
+    // Click event close score pop-up
+    $("#close").click(function(){
+        $("#score").css("display", "none");
+        $("#button").css("display", "inline-block");
+     
+    });
+
+});
+
+    "use strict"
+class Circle {
+constructor(x,y,r,c,v,e){
+this.x=x;
+this.y=y;
+this.r=r;
+this.c=c;
+this.v=v;
+this.e=e;
+this.a=.2;
+}
+draw(){this.sn+=.3
+       this.y-=this.v;
+       this.y<this.e?this.a-=.02:'';
+       san.beginPath();
+       san.arc(this.x,this.y,this.r,0,44/7,false);
+       san.fillStyle=`rgb(0,0,240)`;
+       san.fill()
+       san.closePath();
+}
+static push(n){var rad=Mnx(1,5),cx=Mnx(rad,W-rad),cy=n?Mnx(H+rad,H+rad+100):Mnx(H*1.2,H*.3),cl=Mnx(100,250),vl=Mnx(.5,2),en=Mnx(H*.8,H*.25);
+return new Circle(cx,cy,rad,cl,vl,en)}
+}
+var Mnx=(n,x)=>{return Math.random()*(x-n)+n},W=window.innerWidth,H=window.innerHeight,san;
+var ar=[];
+for(var i=0;i<550;i++){
+ar.push(Circle.push(false))
+}
+
+var draw=()=>{for(var i=0;i<ar.length;i++){
+ar[i].draw();if(ar[i].a<0){ar[i]=Circle.push(true)};}}
+var init=()=>{
+var can=document.querySelector('canvas');
+can.width=W;can.height=H;can.style.background='white';can.style.position='fixed';can.style.left=0;can.style.top=0;window.backdropFilter='blur(2px)';san=can.getContext('2d');
+var up=()=>{
+//san.fillStyle=`rgba(0,0,0,.1)`
+san.clearRect(0,0,W,H);
+draw();webkitRequestAnimationFrame(up);};
+webkitRequestAnimationFrame(up);
+};
+onload=init;
