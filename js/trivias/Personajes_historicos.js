8baf0127bb2c7f51f5d773d381216edcc69b42e9
Holman Morales <hmoralesguido@gmail.com>
2022-02-23 23:45:41 -0600

    Nueva trivia

--- js/trivias/Personajes_historicos.js
+++ js/trivias/Personajes_historicos.js
@@ -1,83 +1,146 @@
 var QUESTIONS = [
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "1/20",
+        answers: ["Napoleón Bonaparte","Luis XVI"],
+        correctAnswer: "Napoleón Bonaparte",
+        image: "https://c.tenor.com/rxvHL9J8t0oAAAAd/napoleon-bonaparte-singing.gif"
     },
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "2/20",
+        answers: ["Willian Shakespeare","Dante Alighieri"],
+        correctAnswer: "Willian Shakespeare",
+        image: "https://media4.giphy.com/media/oveqQA2LxpwYg/giphy.gif"
     },
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "3/20",
+        answers: ["Wolfgang Amadeus Mozart","George Washington" ],
+        correctAnswer: "Wolfgang Amadeus Mozart",
+        image: "https://c.tenor.com/rEJ7NFj5Os4AAAAC/mozart-wink.gif"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "4/20",
+        answers: ["Luis XVI","Enrique VII" ],
+        correctAnswer: "Enrique VII",
+        image: "https://i0.wp.com/www.mavipastor.com/wp-content/uploads/2020/03/enrique-viii-retrato-de-hans_holbein_el-joven.jpg?fit=893%2C1024&ssl=1"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "5/20",
+        answers: ["Aristóteles","Homero"],
+        correctAnswer: "Aristóteles",
+        image: "https://www.abramoscomillas.com/images/aristotelesbiografia205x232.jpg?crc=3931880477"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "6/20",
+        answers: ["Atila","Julio Cesar"],
+        correctAnswer: "Julio Cesar",
+        image: "https://media.vozpopuli.com/2021/12/11/caesar-by-rubens-e1639234694388.jpg"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "7/20",
+        answers: ["Marco Polo","Cristóbal Colón"],
+        correctAnswer: "Cristóbal Colón",
+        image: "https://media.giphy.com/media/T9Q2NTOaP3mQU/giphy.gif"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "8/20",
+        answers: ["Julio Cesar","Alejandro Magno"],
+        correctAnswer: "Alejandro Magno",
+        image: "https://www.biografiasyvidas.com/biografia/a/fotos/alejandro_magno_6.jpg"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "9/20",
+        answers: ["René Descartes","Juana de Arco"],
+        correctAnswer: "Juana de Arco",
+        image: "https://www.radioestrelladelmar.org/wp-content/uploads/2018/05/Piffard_Harold_H_Joan_Of_Arc.jpg"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "10/20",
+        answers: ["Augusto","Leonardo da Vinci"],
+        correctAnswer: "Leonardo da Vinci",
+        image: "http://3.bp.blogspot.com/-yUwAXXJHZp8/UTBivcFpB1I/AAAAAAAAGn0/qylKzdZ-65M/s1600/portrait+leonardo+da+vinci+renacimiento+renaissance+pintor+495.jpg"
     }
     ,
     {
-        question: "",
-        answers: ["", "", "",""],
-        correctAnswer: "",
-        image: ""
+        question: "11/20",
+        answers: ["Genghis Khan","Sun Tzu"],
+        correctAnswer: "Genghis Khan",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/YuanEmperorAlbumGenghisPortrait.jpg/245px-YuanEmperorAlbumGenghisPortrait.jpg"
+    }
+    ,
+    {
+        question: "12/20",
+        answers: ["Galileo Galilei","Friedrich Nietzsche"],
+        correctAnswer: "Galileo Galilei",
+        image: "https://c.tenor.com/uMPBfyipAKQAAAAd/galileo-galilei.gif"
+    }
+    ,
+    {
+        question: "13/20",
+        answers: ["Nefertiti","Cleopatra"],
+        correctAnswer: "Cleopatra",
+        image: "https://historia.nationalgeographic.com.es/medio/2019/12/11/cleopatra_13d2dee2_1280x720.png"
+    }
+    ,
+    {
+        question: "14/20",
+        answers: ["John Adams","Mark Twain"],
+        correctAnswer: "Mark Twain",
+        image: "https://i.pinimg.com/originals/55/74/6d/55746d8729152b4ac5372767017cbcda.gif"
+    }
+    ,
+    {
+        question: "15/20",
+        answers: ["Vincent van Gogh","Claude Monet"],
+        correctAnswer: "Vincent van Gogh",
+        image: "https://c.tenor.com/TLBoeqXBJ7gAAAAd/loving-vincent-paint.gif"
+    }
+    ,
+    {
+        question: "16/20",
+        answers: ["Marie Curie","Frida Kahlo"],
+        correctAnswer: "Frida Kahlo",
+        image: "https://c.tenor.com/8ME5bGnwIqwAAAAd/frida-kahlo.gif"
+    }
+    ,
+    {
+        question: "17/20",
+        answers: ["Nikola Tesla","Miguel Ángel"],
+        correctAnswer: "Miguel Ángel",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Miguel_%C3%81ngel%2C_por_Daniele_da_Volterra_%28detalle%29.jpg"
+    }
+    ,
+    {
+        question: "18/20",
+        answers: ["Emily Brontë","Jane Austen"],
+        correctAnswer: "Emily Brontë",
+        image: "https://www.poemas-del-alma.com/blog/wp-content/uploads/2017/10/emily-bronte-400x348.jpg"
+    }
+    ,
+    {
+        question: "19/20",
+        answers: ["Sigmund Freud","Charles Darwin"],
+        correctAnswer: "Charles Darwin",
+        image: "https://www.biografiasyvidas.com/monografia/darwin/fotos/darwin_1881.jpg"
+    }
+    ,
+    {
+        question: "20/20",
+        answers: ["Benjamin Franklin","Isaac Newton"],
+        correctAnswer: "Isaac Newton",
+        image: "https://i.gifer.com/origin/4d/4db34cea6b43dafb9d682d92deedc895.gif"
     }
 ];
 
 var correctAnswerElement;
 var questionIndex = 0;
-var timeLeft = 15;
+var timeLeft = 10;
 var timer;
 var correct = 0;
 var incorrect = 0;
@@ -195,7 +258,7 @@ $(function () {
                 incorrect++;
               showCorrectAnswertimer();
                 clearInterval(timer);
-                timeLeft = 15;
+                timeLeft = 10;
                 $("#timeLeft").css("display", "none");
                 $("#message").text("se acabó el tiempo!");
                 $("#message").css("display", "block");
@@ -223,7 +286,7 @@ $(function () {
 
         setTimeout(function () {
             questionIndex++;
-            timeLeft = 15;
+            timeLeft = 10;
 			startGame();
             $("#message").css("display", "none");
             $("#timer").css("display", "flex");
@@ -276,6 +339,7 @@ Hora: hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds(),
 Correctas:correct,
 incorrectas:incorrect
 });
+   
    
 
 
