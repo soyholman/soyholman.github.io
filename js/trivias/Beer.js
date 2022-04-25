d42d0de73e9dc56c9e94182d6289fe03524f1a63
Holman <hmoralesguido@gmail.com>
2022-04-24 20:32:34 -0600

    Nueva trivia de cerveza

---
+++ js/trivias/Beer.js
@@ -0,0 +1,342 @@
+var QUESTIONS = [
+  {
+    question: "Los egipcios ya conocían una bebida similar a la cerveza.",
+    answers: ["Falso", "Cierto"],
+    correctAnswer: "Cierto",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/41abdcde-754f-4af6-8430-3c23824fc7d2/ee37ab8b-6d38-4263-8e1e-27b028ccd84c.jpg",
+  },
+  {
+    question: "¿Dónde está la cervecería más antigua del mundo?",
+    answers: ["En Egipto", "En Alemania", "En Irlanda", "En Suiza"],
+    correctAnswer: "En Alemania",
+    image:
+      "https://images.unsplash.com/photo-1444988510113-d8e9c6741202?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+  },
+  {
+    question: "¿Quiénes hacían la cerveza durante la edad media?",
+    answers: [
+      "Los monjes",
+      "Los reyes",
+      "Las vírgenes",
+      "Las dueñas de los burdeles",
+    ],
+    correctAnswer: "Los monjes",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/41abdcde-754f-4af6-8430-3c23824fc7d2/e6147696-e90a-4dbb-80bf-1488394589de.jpg",
+  },
+  {
+    question: "¿Cuál es el grupo cervecero más grande del mundo?",
+    answers: ["SAB Miller!", "Modelo", "Heineken", "Belg Brew"],
+    correctAnswer: "Heineken",
+    image:
+      "https://images.unsplash.com/photo-1648142375707-03918153f53e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80",
+  },
+  {
+    question:
+      "¿Cuánto bebe por persona al año el país más cervecero del mundo?",
+    answers: ["159.3 litros", "99 galones", "432 litros", "243.9 litros"],
+    correctAnswer: "159.3 litros",
+    image:
+      "https://images.unsplash.com/photo-1541020230467-e33e4f96f7fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    question:
+      "¿Cuántos grados de alcohol tiene la cerveza más fuerte del mundo?  ",
+    answers: ["72%", "69.9%", "43.7%", "67.5%"],
+    correctAnswer: "67.5%",
+    image:
+      "https://images.unsplash.com/photo-1535958636474-b021ee887b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+
+  {
+    question:
+      "En Estados Unidos la ley prohibe que las personas tomen cerveza en los anuncios",
+    answers: ["Falso", "Cierto"],
+    correctAnswer: "Cierto",
+    image:
+      "https://images.unsplash.com/photo-1471421298428-1513ab720a8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80",
+  },
+  {
+    question: "Existe una cerveza dedicada al papa",
+    answers: ["Falso", "Cierto"],
+    correctAnswer: "Cierto",
+    image:
+      "https://images.unsplash.com/photo-1603666413723-fd8b35434cba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 15;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+  // Function gets Questions-Data and append it to HTML and
+  // returns element containing correct answer
+  function getTrivia() {
+    var correctAnswer;
+    $("#answers").empty();
+    $("#question-header").text(QUESTIONS[questionIndex].question);
+    QUESTIONS[questionIndex].answers.forEach(function (answer) {
+      var div = $("<div>").append(answer);
+      div.appendTo($("#answers"));
+      if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+        correctAnswer = div;
+      }
+      div.addClass("hover");
+      $("#Titles").css("font-size", "medium"); // Adds hover effect to each answer element
+    });
+
+    return correctAnswer;
+  }
+
+  // Function adds click event to each answer element and
+  // checks for correct answer. If clicked element has
+  // correct answer, text turns green and appends check mark
+  // else text turns color gray and appends an X mark
+  function checkCorrectAnswer() {
+    $("#answers")
+      .children()
+      .click(function () {
+        resetIntervalTimer();
+
+        if ($(this).text() === correctAnswerElement.text()) {
+          correct++;
+          showCorrectAnswer();
+        } else {
+          incorrect++;
+          $(this).addClass("wrong");
+          $(this).append("<span> &#10008;</span>");
+          $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+          correctAnswerElement.append("<span> &#10004;</span>");
+          correctAnswerElement.addClass("correct");
+
+          // $("#message").css("display", "block");
+          $("#timeLeft").css("display", "none");
+          $("#message").text("INCORRECTO");
+
+          $("#imageTrivia").attr(
+            "src",
+            "https://media.baamboozle.com/uploads/images/49869/1618223208_15917_gif-url.gif"
+          );
+        }
+
+        $(this).parent().children().off("click"); // Prevent click event
+        $("#answers").children().removeClass("hover"); // Turn off hover effect
+      });
+  }
+
+  // Function shows the correct trivia answer
+  function showCorrectAnswer() {
+    // $("#answers").children().not(correctAnswerElement).addClass("wrong");
+    // correctAnswerElement.append("<span> &#10004;</span>");
+    // correctAnswerElement.addClass("correct");
+
+    // $("#answers").children().removeClass("hover");
+    // $("#answers").children().off("click");
+
+    // $("#message").css("display", "block");
+    // $("#timer").css("display", "block");
+    // $("#message").text("CORRECTO");
+
+    // $("#imageTrivia").attr("src", "https://c.tenor.com/BntXpMlrGuEAAAAC/check-correct.gif");
+    Swal.fire({
+      icon: "success",
+      title: "Correcto",
+      showConfirmButton: false,
+      timer: 1600,
+    });
+  }
+
+  // Function creates a countdown which is display on HTML
+  // If timeLeft variable reach zero, correct answer shows up
+  // and then wait for 4 seconds to restart
+  function intervalTimer() {
+    timer = setInterval(function () {
+      if (timeLeft == 0) {
+        incorrect++;
+        showCorrectAnswer();
+        clearInterval(timer);
+
+        $("#timer").css("display", "none");
+        $("#message").text("se acabó el tiempo!");
+        $("#message").css("display", "block");
+
+        setTimeout(function () {
+          questionIndex++;
+          startGame();
+          $("#message").css("display", "none");
+          $("#timeLeft").css("display", "block");
+          $("#timerSeconds").text(timeLeft);
+          $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+        }, 5000);
+      }
+    }, 1000);
+  }
+
+  // Function clears countdown after an answer is click and
+  // waits four seconds to restart
+  function resetIntervalTimer() {
+    clearInterval(timer);
+
+    setTimeout(function () {
+      questionIndex++;
+
+      startGame();
+      $("#message").css("display", "none");
+      $("#timer").css("display", "block");
+      $("#timeLeft").css("display", "block");
+
+      $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+    }, 2000);
+  }
+
+  function resetInervalfalse() {
+    questionIndex++;
+
+    startGame();
+    $("#message").css("display", "none");
+
+    $("#timeLeft").css("display", "block");
+
+    $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+  }
+
+  // Function starts the game
+  // If questionIndex reach QUESTIONS length, game ends and score
+  // shows up, otherwise the games keeps running until last question
+  function startGame() {
+    if (questionIndex < QUESTIONS.length) {
+      correctAnswerElement = getTrivia();
+
+      checkCorrectAnswer();
+
+      intervalTimer();
+    } else {
+      questionIndex = 0;
+      $("#card").css("display", "none");
+      $("#correct").text("Correcto: " + correct);
+      $("#incorrect").text("Incorrecto: " + incorrect);
+      $("#score").css("display", "block");
+      var user = firebase.auth().currentUser;
+      auth.onAuthStateChanged((user) => {
+        var hoy = new Date();
+        var dd = hoy.getDate();
+        var mm = hoy.getMonth() + 1;
+        var yyyy = hoy.getFullYear();
+        var data_carpet = dd + "_" + mm + "_" + yyyy;
+
+        var firebaseref = firebase
+          .database()
+          .ref(
+            "Trivia/" +
+              document.getElementById("Titles").textContent +
+              "/" +
+              data_carpet +
+              "/" +
+              user.displayName
+          );
+        const fs = firebase.firestore();
+
+        firebaseref.push({
+          // fecha:dd+'/'+mm+'/'+yyyy,
+          usuario: user.displayName,
+          Hora:
+            hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds(),
+          Correctas: correct,
+          incorrectas: incorrect,
+        });
+      });
+    }
+  }
+
+  // Click event button starts game
+  $("#button").click(function () {
+    correct = 0;
+    incorrect = 0;
+    questionIndex = 0;
+    $("#card").css("display", "flex");
+    $("#button").css("display", "none");
+    startGame();
+  });
+
+  // Click event close score pop-up
+  $("#close").click(function () {
+    $("#score").css("display", "none");
+    $("#button").css("display", "inline-block");
+  });
+});
+
+("use strict");
+class Circle {
+  constructor(x, y, r, c, v, e) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.c = c;
+    this.v = v;
+    this.e = e;
+    this.a = 0.2;
+  }
+  draw() {
+    this.sn += 0.3;
+    this.y -= this.v;
+    this.y < this.e ? (this.a -= 0.02) : "";
+    san.beginPath();
+    san.arc(this.x, this.y, this.r, 0, 44 / 7, false);
+    san.fillStyle = `rgb(0,0,240)`;
+    san.fill();
+    san.closePath();
+  }
+  static push(n) {
+    var rad = Mnx(1, 5),
+      cx = Mnx(rad, W - rad),
+      cy = n ? Mnx(H + rad, H + rad + 100) : Mnx(H * 1.2, H * 0.3),
+      cl = Mnx(100, 250),
+      vl = Mnx(0.5, 2),
+      en = Mnx(H * 0.8, H * 0.25);
+    return new Circle(cx, cy, rad, cl, vl, en);
+  }
+}
+var Mnx = (n, x) => {
+    return Math.random() * (x - n) + n;
+  },
+  W = window.innerWidth,
+  H = window.innerHeight,
+  san;
+var ar = [];
+for (var i = 0; i < 550; i++) {
+  ar.push(Circle.push(false));
+}
+
+var draw = () => {
+  for (var i = 0; i < ar.length; i++) {
+    ar[i].draw();
+    if (ar[i].a < 0) {
+      ar[i] = Circle.push(true);
+    }
+  }
+};
+var init = () => {
+  var can = document.querySelector("canvas");
+  can.width = W;
+  can.height = H;
+  can.style.background = "white";
+  can.style.position = "fixed";
+  can.style.left = 0;
+  can.style.top = 0;
+  window.backdropFilter = "blur(2px)";
+  san = can.getContext("2d");
+  var up = () => {
+    //san.fillStyle=`rgba(0,0,0,.1)`
+    san.clearRect(0, 0, W, H);
+    draw();
+    webkitRequestAnimationFrame(up);
+  };
+  webkitRequestAnimationFrame(up);
+};
+onload = init;
