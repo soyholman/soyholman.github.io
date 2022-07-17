2229aa221257f0699bf0086b68b1cde3f9cc9e46
Holman <hmoralesguido@gmail.com>
2022-07-16 21:44:17 -0600

    Nueva trivia world

---
+++ js/trivias/world.js
@@ -0,0 +1,351 @@
+var QUESTIONS = [
+  {
+    question: "¿Cuál es el país con la mayor cantidad de elefantes?",
+    answers: ["Indonesia", "Tailandia", "India"],
+    correctAnswer: "India",
+    image:
+      "https://i.pinimg.com/736x/a4/56/a9/a456a912d399e1ae04c89dba909f626f.jpg",
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    answers: ["Puerto Rico", "Cuba"],
+    correctAnswer: "Puerto Rico",
+    image:
+      "https://i.pinimg.com/564x/12/bf/ee/12bfee8a7d264254d47c66baf1b620b5.jpg",
+  },
+  {
+    question: "¿Dónde ocurre el fenómeno del Relámpago del Catatumbo?",
+    answers: ["Japón", "Chile", "Venezuela"],
+    correctAnswer: "Venezuela",
+    image:
+      "https://i.pinimg.com/564x/a3/31/f6/a331f6201968641fec63b1daceed37f2.jpg",
+  },
+  {
+    question: "¿Dónde queda el templo de Shwedagon?",
+    answers: ["Nepal", "Birmania", "China"],
+    correctAnswer: "Birmania",
+    image:
+      "https://i.pinimg.com/564x/36/33/55/3633558f8d0fa99af4f8b2cda37adae1.jpg",
+  },
+  {
+    question: "¿Cuál es el país con mas bares?",
+    answers: ["Alemania", "España", "México"],
+    correctAnswer: "España",
+    image:
+      "https://i.pinimg.com/564x/bd/4b/8a/bd4b8a7ae23fee36ec1802b9a948beb6.jpg",
+  },
+  {
+    question: "¿Cuál es esta ciudad?",
+    answers: ["Bankok", "Hong Kong", "Venecia"],
+    correctAnswer: "Bankok",
+    image:
+      "https://i.pinimg.com/564x/01/e5/d7/01e5d7658bdfcb2857d0714805121344.jpg",
+  },
+  {
+    question: "¿País con más castillos por metro cuadrado?",
+    answers: ["Rusia", "Suiza", "Portugal", "Bélgica"],
+    correctAnswer: "Bélgica",
+    image:
+      "https://i.pinimg.com/564x/fd/ff/25/fdff25677ead425c45e27115ddd1a86b.jpg",
+  },
+  {
+    question: "¿Cuál es la capital de Canadá?",
+    answers: ["Toronto", "Montreal", "Ottawa"],
+    correctAnswer: "Ottawa",
+    image:
+      "https://i.pinimg.com/564x/59/a1/86/59a18673dd27744b13ac8199419ff0fe.jpg  ",
+  },
+  {
+    question: "País con menos días feriados",
+    answers: ["Japón", "Noruega", "México", "Australia"],
+    correctAnswer: "México",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/cdn/bbfb0c64-1aff-47d9-8c55-5d5e1bb15b90/a14520b1-a8c5-4050-98b0-81a4b7795402.jpg",
+  },
+  {
+    question: "¿Cuál es la ciudad habitada más antigua?",
+    answers: ["Jerusalén", "Alepo", "Jericó"],
+    correctAnswer: "Jericó",
+    image:
+      "https://i.pinimg.com/564x/ba/ab/fe/baabfe3817a4e52e34a5edd73cf908a5.jpg",
+  },
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 15;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+  // Function gets Questions-Data and append it to HTML and
+  // returns element containing correct answer
+  function getTrivia() {
+    var correctAnswer;
+    $("#answers").empty();
+    $("#question-header").text(QUESTIONS[questionIndex].question);
+    QUESTIONS[questionIndex].answers.forEach(function (answer) {
+      var div = $("<div>").append(answer);
+      div.appendTo($("#answers"));
+      if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+        correctAnswer = div;
+      }
+      div.addClass("hover");
+      $("#Titles").css("font-size", "medium"); // Adds hover effect to each answer element
+    });
+
+    return correctAnswer;
+  }
+
+  // Function adds click event to each answer element and
+  // checks for correct answer. If clicked element has
+  // correct answer, text turns green and appends check mark
+  // else text turns color gray and appends an X mark
+  function checkCorrectAnswer() {
+    $("#answers")
+      .children()
+      .click(function () {
+        resetIntervalTimer();
+
+        if ($(this).text() === correctAnswerElement.text()) {
+          correct++;
+          showCorrectAnswer();
+        } else {
+          incorrect++;
+          $(this).addClass("wrong");
+          $(this).append("<span> &#10008;</span>");
+          $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+          correctAnswerElement.append("<span> &#10004;</span>");
+          correctAnswerElement.addClass("correct");
+
+          $("#message").css("display", "block");
+          $("#timeLeft").css("display", "none");
+          $("#message").text("INCORRECTO");
+
+          $("#imageTrivia").attr(
+            "src",
+            "https://media.baamboozle.com/uploads/images/49869/1618223208_15917_gif-url.gif"
+          );
+        }
+
+        $(this).parent().children().off("click"); // Prevent click event
+        $("#answers").children().removeClass("hover"); // Turn off hover effect
+      });
+  }
+
+  // Function shows the correct trivia answer
+  function showCorrectAnswer() {
+    $("#answers").children().not(correctAnswerElement).addClass("wrong");
+    correctAnswerElement.append("<span> &#10004;</span>");
+    correctAnswerElement.addClass("correct");
+
+    $("#answers").children().removeClass("hover");
+    $("#answers").children().off("click");
+    $("#timer").css("display", "none");
+    $("#message").css("display", "none");
+    $("#timeLeft").css("display", "none");
+    // $("#message").text("CORRECTO");
+
+    Swal.fire({
+      icon: "success",
+      title: "Correcto",
+      showConfirmButton: false,
+      timer: 1600,
+    });
+  }
+  function showCorrectAnswertimer() {
+    $("#answers").children().not(correctAnswerElement).addClass("wrong");
+    correctAnswerElement.append("<span> &#10004;</span>");
+    correctAnswerElement.addClass("correct");
+
+    $("#answers").children().removeClass("hover");
+    $("#answers").children().off("click");
+
+    $("#message").css("display", "none");
+    $("#timeLeft").css("display", "none");
+    // $("#message").text("CORRECTO");
+  }
+
+  // Function creates a countdown which is display on HTML
+  // If timeLeft variable reach zero, correct answer shows up
+  // and then wait for 4 seconds to restart
+  function intervalTimer() {
+    timer = setInterval(function () {
+      timeLeft--;
+      $("#timerSeconds").text(timeLeft);
+
+      if (timeLeft == 0) {
+        incorrect++;
+        showCorrectAnswertimer();
+        clearInterval(timer);
+        timeLeft = 15;
+        $("#timeLeft").css("display", "none");
+        $("#message").text("se acabó el tiempo!");
+        $("#message").css("display", "block");
+
+        setTimeout(function () {
+          questionIndex++;
+          startGame();
+          $("#message").css("display", "none");
+          $("#timeLeft").css("display", "block");
+          $("#timerSeconds").text(timeLeft);
+          $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+        }, 5000);
+      }
+    }, 1000);
+  }
+
+  // Function clears countdown after an answer is click and
+  // waits four seconds to restart
+  function resetIntervalTimer() {
+    clearInterval(timer);
+
+    setTimeout(function () {
+      questionIndex++;
+      timeLeft = 15;
+      startGame();
+      $("#message").css("display", "none");
+      $("#timer").css("display", "flex");
+      $("#message").text("SE HA TERMINADO EL TIEMPO!");
+      $("#timeLeft").css("display", "block");
+      $("#timerSeconds").text(timeLeft);
+      $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+    }, 2000);
+  }
+
+  // Function starts the game
+  // If questionIndex reach QUESTIONS length, game ends and score
+  // shows up, otherwise the games keeps running until last question
+  function startGame() {
+    if (questionIndex < QUESTIONS.length) {
+      correctAnswerElement = getTrivia();
+
+      checkCorrectAnswer();
+
+      intervalTimer();
+    } else {
+      questionIndex = 0;
+      $("#card").css("display", "none");
+      $("#correct").text("Correcto: " + correct);
+      $("#incorrect").text("Incorrecto: " + incorrect);
+      $("#score").css("display", "block");
+      var user = firebase.auth().currentUser;
+      auth.onAuthStateChanged((user) => {
+        var hoy = new Date();
+        var dd = hoy.getDate();
+        var mm = hoy.getMonth() + 1;
+        var yyyy = hoy.getFullYear();
+        var data_carpet = dd + "_" + mm + "_" + yyyy;
+
+        var firebaseref = firebase
+          .database()
+          .ref(
+            "Trivia/" +
+              document.getElementById("Titles").innerText +
+              "/" +
+              data_carpet +
+              "/" +
+              user.displayName
+          );
+        const fs = firebase.firestore();
+
+        firebaseref.push({
+          // fecha:dd+'/'+mm+'/'+yyyy,
+          usuario: user.displayName,
+          Hora:
+            hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds(),
+          Correctas: correct,
+          incorrectas: incorrect,
+        });
+      });
+    }
+  }
+
+  // Click event button starts game
+  $("#button").click(function () {
+    correct = 0;
+    incorrect = 0;
+    questionIndex = 0;
+    $("#card").css("display", "flex");
+    $("#button").css("display", "none");
+    startGame();
+  });
+
+  // Click event close score pop-up
+  $("#close").click(function () {
+    $("#score").css("display", "none");
+    $("#button").css("display", "inline-block");
+  });
+});
+
+("use strict");
+class Circle {
+  constructor(x, y, r, c, v, e) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.c = c;
+    this.v = v;
+    this.e = e;
+    this.a = 0.2;
+  }
+  draw() {
+    this.sn += 0.3;
+    this.y -= this.v;
+    this.y < this.e ? (this.a -= 0.02) : "";
+    san.beginPath();
+    san.arc(this.x, this.y, this.r, 0, 44 / 7, false);
+    san.fillStyle = `rgb(0,0,240)`;
+    san.fill();
+    san.closePath();
+  }
+  static push(n) {
+    var rad = Mnx(1, 5),
+      cx = Mnx(rad, W - rad),
+      cy = n ? Mnx(H + rad, H + rad + 100) : Mnx(H * 1.2, H * 0.3),
+      cl = Mnx(100, 250),
+      vl = Mnx(0.5, 2),
+      en = Mnx(H * 0.8, H * 0.25);
+    return new Circle(cx, cy, rad, cl, vl, en);
+  }
+}
+var Mnx = (n, x) => {
+    return Math.random() * (x - n) + n;
+  },
+  W = window.innerWidth,
+  H = window.innerHeight,
+  san;
+var ar = [];
+for (var i = 0; i < 550; i++) {
+  ar.push(Circle.push(false));
+}
+
+var draw = () => {
+  for (var i = 0; i < ar.length; i++) {
+    ar[i].draw();
+    if (ar[i].a < 0) {
+      ar[i] = Circle.push(true);
+    }
+  }
+};
+var init = () => {
+  var can = document.querySelector("canvas");
+  can.width = W;
+  can.height = H;
+  can.style.background = "white";
+  can.style.position = "fixed";
+  can.style.left = 0;
+  can.style.top = 0;
+  window.backdropFilter = "blur(2px)";
+  san = can.getContext("2d");
+  var up = () => {
+    //san.fillStyle=`rgba(0,0,0,.1)`
+    san.clearRect(0, 0, W, H);
+    draw();
+    webkitRequestAnimationFrame(up);
+  };
+  webkitRequestAnimationFrame(up);
+};
+onload = init;
