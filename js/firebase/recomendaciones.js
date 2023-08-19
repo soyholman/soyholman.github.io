d49f6fa4b26a0ff8f1ab20f09feef98ef0cd2605
Holman <84192726+soyholman@users.noreply.github.com>
2023-08-19 13:25:43 -0600

    Seo improvements and new test

--- js/firebase/recomendaciones.js
+++ js/firebase/recomendaciones.js
@@ -1,73 +1,56 @@
-
 var user = firebase.auth().currentUser;
-auth.onAuthStateChanged(user=>{
+auth.onAuthStateChanged((user) => {
+  const nombre = document.getElementById("getname");
 
-const nombre=document.getElementById('getname');
+  var hoy = new Date();
+  var dd = hoy.getDate();
+  var mm = hoy.getMonth() + 1;
+  var yyyy = hoy.getFullYear();
+  var data_carpet = dd + "_" + mm + "_" + yyyy;
 
-var hoy = new Date();
-var dd = hoy.getDate();
-var mm = hoy.getMonth()+1;
-var yyyy = hoy.getFullYear();
-var data_carpet=dd+'_'+mm+'_'+yyyy;
+  if (user.displayName == null) {
+    nombre.textContent = "";
+  } else if (user.displayName != null) {
+    nombre.textContent = ":" + user.displayName + "";
+  }
+  var userPicURL = user.photoURL;
 
+  document.querySelector(".avatar__image").src = user.photoURL;
+  var firebaseref = firebase
+    .database()
+    .ref("Sugerencias/" + user.displayName + "/" + data_carpet.toString());
+  const fs = firebase.firestore();
 
-if(user.displayName==null){
-      nombre.textContent='';   
-}
-else if(user.displayName!=null){
-      nombre.textContent=':' +user.displayName+'';   
-}
-var userPicURL = user.photoURL; 
+  document.querySelector("#Enviar").addEventListener("click", () => {
+    const dinamica = document.getElementById("dinamic");
+    const sugerencia = document.getElementById("suggest").value;
 
-document.querySelector('.avatar__image').src =  user.photoURL;
-var firebaseref=firebase.database().ref('Sugerencias/'+user.displayName+"/"+ data_carpet.toString());
-const fs=firebase.firestore();
-
-document.querySelector("#Enviar").addEventListener("click",()=>{
-const dinamica= document.getElementById('dinamic');
-const sugerencia=document.getElementById('suggest').value;
-
-
-
-if(dinamica=="" || sugerencia==""){
-
-Swal.fire({
-       
-       icon: 'error',
-       title: 'Por favor seleccione la dinamica y escriba el tema a sugerir.',
-       text: '',
-       showConfirmButton: false,
-       timer:1600
-      
-     })
-
-}
-else{
-
-
-
-Swal.fire({
-       
-       icon: 'success',
-       title: 'Se ha enviado tu sugerencia',
-       text: 'Muchas Gracias',
-       showConfirmButton: false,
-      timer:1600
-     }).then(function(){
-
+    if (dinamica == "" || sugerencia == "") {
+      Swal.fire({
+        icon: "error",
+        title: "Por favor seleccione la dinamica y escriba el tema a sugerir.",
+        text: "",
+        showConfirmButton: false,
+        timer: 1600,
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        title: "Se ha enviado tu sugerencia",
+        text: "Muchas Gracias",
+        showConfirmButton: false,
+        timer: 1600,
+      }).then(function () {
         firebaseref.push({
-fecha:dd+'/'+mm+'/'+yyyy,
-dinamica:dinamica.value,
-usuario:user.displayName,
-correo:user.email,
-      tema:sugerencia
+          fecha: dd + "/" + mm + "/" + yyyy,
+          dinamica: dinamica.value,
+          usuario: user.displayName,
+          correo: user.email,
+          tema: sugerencia,
+        });
+        document.getElementById("suggest").value = "";
+        console.log(nombre.textContent);
+      });
+    }
+  });
 });
-document.getElementById('suggest').value = ''
-console.log(nombre.textContent);
-  
-})
-
-}
-
-});
-})
---
+++ js/test/Trivia_DosMentirasUnaVerdad.ht.js
@@ -0,0 +1,410 @@
+var QUESTIONS = [
+  {
+    question: "¿Cuál de estas opciones es verdad acerca de «Kiribati»? ",
+    text: "Esta es una nación que está formada por numerosas islas pequeñas ubicadas en el océano Pacífico. Su territorio cubre los cuatro hemisferios de la Tierra (norte, sur, este y oeste). ",
+    answers: [
+      "Es una palabra inexistente ",
+      "Es un país",
+      "Tribu indígena americana",
+    ],
+    correctAnswer: "Es un país",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557258719/fde52ofmmcsai3ltjvet.jpg",
+  },
+  {
+    question: "Escoge la verdad entre estas opciones…",
+    text: "Este nombre es diminutivo de Gwendolyn. Apareció por primera vez en el libro Peter Pan y posteriormente fue popularizado por la adaptación al cine de esta obra por parte de Disney.",
+    answers: [
+      "Te puedes matar conteniendo el aliento por ti mismo",
+      "Los meses que empiezan en martes, siempre tendrán un viernes 13",
+      "El nombre Wendy se inventó en el libro Peter Pan",
+    ],
+    correctAnswer: "El nombre Wendy se inventó en el libro Peter Pan",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557258855/d4ynep5xpebidkrrxvgd.jpg",
+  },
+  {
+    question: "¿Cuál de estas curiosidades será cierta en estos países? ",
+    text: "Esta pequeña nación insular del océano Pacífico posee monedas con personajes de Disney y Star Wars con un valor de 25 dólares en la moneda de Niue. ",
+    answers: [
+      "Groenlandia tiene representación en la FIFA ",
+      "Etiopía sigue un calendario que está 10 años atrás del resto del mundo ",
+      "Niue tiene monedas con personajes de Disney",
+    ],
+    correctAnswer: "Niue tiene monedas con personajes de Disney",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557259045/pf90hpcgl3bpnuemqiqi.jpg",
+  },
+  {
+    question: "¡Veamos si aciertas en esta!",
+    text: "Esto es porque era muy celosa y tenía la creencia que una examante podía robárselo.",
+    answers: [
+      "Los pingüinos no vuelan porque no tienen plumas ",
+      "Juana la loca, mantuvo varios meses sin enterrar el cuerpo de su marido, luego de que este falleció  ",
+      "Los jueces se visten de negro porque representa el fin  ",
+    ],
+    correctAnswer:
+      "Juana la loca, mantuvo varios meses sin enterrar el cuerpo de su marido, luego de que este falleció  ",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557259325/sp0lmw9s82dh48xbp5g7.jpg",
+  },
+  {
+    question: "Escoge la verdad entre estas opciones sobre animales…  ",
+    text: "Estos reptiles no pueden sacar la lengua más de unos centímetros, debido a que su lengua se encuentra unida al paladar por una membrana. ",
+    answers: [
+      "Los cocodrilos no pueden sacar la lengua",
+      "El vuelo registrado de un pollo es de 10 segundos  ",
+      "Ni los gatos, ni los perros pueden ser zurdos ",
+    ],
+    correctAnswer: "Los cocodrilos no pueden sacar la lengua",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557259480/mx66vpw4g3opdih7jqzb.jpg",
+  },
+  {
+    question: "¡Otra de países! ¿Cuál será verdad?",
+    text: "La localidad de Fucking, Austria, ostenta el récord de poseer no solo la señal de carretera más fotografiada, sino más robada del mundo. ",
+    answers: [
+      "El país Nauru tiene dos capitales ",
+      "En Austria se encuentra la señal de carretera más fotografiada ",
+      "La mayoría de la población en Guatemala habla lenguas indígenas ",
+    ],
+    correctAnswer:
+      "En Austria se encuentra la señal de carretera más fotografiada ",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557259650/c7mmfdcdw0o1dtx7rmqu.jpg",
+  },
+  {
+    question:
+      "Escoge lo que verdaderamente sucedió entre estos hechos históricos… ",
+    text: "Catalina Howard -quinta esposa de Enrique VIII- pasó horas practicando cómo colocar su cabeza sobre el cadalso. ",
+    answers: [
+      "Jesucristo probablemente nació en el año 2 a. C.  ",
+      "Una reina de Inglaterra pidió ensayar su decapitación",
+      "En Alemania se usaba la sangre menstrual de las mujeres como medicina ",
+    ],
+    correctAnswer: "Una reina de Inglaterra pidió ensayar su decapitación",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557259805/gow7e4gmmunqrkhnsxbc.jpg",
+  },
+  {
+    question: "Escoge la opción correcta…",
+    text: "Los jaguares tienen tanta potencia en sus mandíbulas que matan a sus presas mordiendo directamente su cabeza, llegando al cerebro a través del cráneo.",
+    answers: [
+      "Los jaguares atacan directamente la cabeza de su oponente ",
+      "Las huellas dactilares no son únicas",
+      "La humanidad es la única especie que practica sexo por placer ",
+    ],
+    correctAnswer: "Los jaguares atacan directamente la cabeza de su oponente ",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557260008/rxjlhez7ac0qlqjffhik.jpg",
+  },
+  {
+    question: "Una de estas opciones es verdad… ",
+    text: "En las islas volcánicas de Aogashima, se encuentra el planetario natural más increíble del mundo, ya que en kilómetros a la redonda no existe ni una pizca de contaminación lumínica.",
+    answers: [
+      "La base de la gran pirámide de Egipto equivale al tamaño de 11 campos de fútbol ",
+      "Existe un planetario natural ",
+      "Papa Noel siempre ha vestido de rojo",
+    ],
+    correctAnswer: "Existe un planetario natural ",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557260136/i9yzam5nl0mmfbrgjqrd.jpg",
+  },
+  {
+    question: "Finalmente… ¿Cuál de estas opciones sí es cierta?",
+    text: "Esta galaxia se encuentra a 2.5 millones de años luz, lo que significa que puede ser vista en lugares muy oscuros y despejados.",
+    answers: [
+      "Todas las letras del alfabeto están en la tabla periódica  ",
+      "La galleta de la fortuna fue inventada por un repostero",
+      "La galaxia de Andrómeda es el objeto más lejano que puede verse a simple vista",
+    ],
+    correctAnswer:
+      "La galaxia de Andrómeda es el objeto más lejano que puede verse a simple vista",
+    image:
+      "https://img.playbuzz.com/image/upload/ar_1.5,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1557260333/hvjgfxjwe3szx1ith2jf.jpg",
+  },
+];
+
+var correctAnswerElement;
+var questionIndex = 0;
+var timeLeft = 15;
+var timer;
+var correct = 0;
+var incorrect = 0;
+
+$(function () {
+  // Function gets Questions-Data and append it to HTML and
+  // returns element containing correct answer
+  function getTrivia() {
+    var correctAnswer;
+    $("#answers").empty();
+    $("#question-header").text(QUESTIONS[questionIndex].question);
+    QUESTIONS[questionIndex].answers.forEach(function (answer) {
+      var div = $("<div>").append(answer);
+      div.appendTo($("#answers"));
+      if (div.text() === QUESTIONS[questionIndex].correctAnswer) {
+        correctAnswer = div;
+      }
+      div.addClass("hover");
+      $("#Titles").css("font-size", "medium"); // Adds hover effect to each answer element
+    });
+
+    return correctAnswer;
+  }
+
+  // Function adds click event to each answer element and
+  // checks for correct answer. If clicked element has
+  // correct answer, text turns green and appends check mark
+  // else text turns color gray and appends an X mark
+  function checkCorrectAnswer() {
+    $("#answers")
+      .children()
+      .click(function () {
+        if ($(this).text() === correctAnswerElement.text()) {
+          resetIntervalTimer();
+          correct++;
+          showCorrectAnswer();
+        } else {
+          resetIntervalTimer();
+          Swal.fire({
+            icon: "error",
+            title: "Respuesta Incorrecta",
+            html: "<small><p>" + QUESTIONS[questionIndex].text + "<p></strong>",
+            footer:
+              '<strong>Respuesta correcta:<p style="color:mediumseagreen;justify-content: center;">' +
+              QUESTIONS[questionIndex].correctAnswer +
+              "<p> </strong>",
+          }).then(function () {
+            incorrect++;
+
+            $(this).addClass("wrong");
+            $(this).append("<span> &#10008;</span>");
+            $(this).siblings().not(correctAnswerElement).addClass("wrong");
+
+            correctAnswerElement.append("<span> &#10004;</span>");
+            correctAnswerElement.addClass("correct");
+
+            $("#message").css("display", "block");
+            $("#message").css("background", "red");
+            $("#timeLeft").css("display", "none");
+            $("#message").text("INCORRECTO");
+
+            // $("#imageTrivia").attr("src", "https://images.angelpub.com/2016/35/39703/wrong.gif");
+          });
+        }
+
+        $(this).parent().children().off("click"); // Prevent click event
+        $("#answers").children().removeClass("hover"); // Turn off hover effect
+      });
+  }
+
+  // Function shows the correct trivia answer
+  function showCorrectAnswer() {
+    // $("#answers").children().not(correctAnswerElement).addClass("wrong");
+    // correctAnswerElement.append("<span> &#10004;</span>");
+    // correctAnswerElement.addClass("correct");
+
+    // $("#answers").children().removeClass("hover");
+    // $("#answers").children().off("click");
+
+    // $("#message").css("display", "block");
+    // $("#timer").css("display", "block");
+    // $("#message").text("CORRECTO");
+
+    // $("#imageTrivia").attr("src", "https://c.tenor.com/BntXpMlrGuEAAAAC/check-correct.gif");
+    Swal.fire({
+      icon: "success",
+      title: "Correcto",
+      showConfirmButton: false,
+      timer: 1600,
+    });
+  }
+
+  // Function creates a countdown which is display on HTML
+  // If timeLeft variable reach zero, correct answer shows up
+  // and then wait for 4 seconds to restart
+  function intervalTimer() {
+    timer = setInterval(function () {
+      if (timeLeft == 0) {
+        incorrect++;
+        showCorrectAnswer();
+        clearInterval(timer);
+
+        $("#timer").css("display", "none");
+        $("#message").text("se acabó el tiempo!");
+        $("#message").css("display", "block");
+
+        setTimeout(function () {
+          questionIndex++;
+          startGame();
+          $("#message").css("display", "none");
+          $("#timeLeft").css("display", "block");
+          $("#timerSeconds").text(timeLeft);
+          $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+        }, 5000);
+      }
+    }, 1000);
+  }
+
+  // Function clears countdown after an answer is click and
+  // waits four seconds to restart
+  function resetIntervalTimer() {
+    clearInterval(timer);
+
+    setTimeout(function () {
+      questionIndex++;
+
+      startGame();
+      $("#message").css("display", "none");
+      $("#timer").css("display", "block");
+      $("#timeLeft").css("display", "block");
+
+      $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+    }, 2000);
+  }
+
+  function resetInervalfalse() {
+    questionIndex++;
+
+    startGame();
+    $("#message").css("display", "none");
+
+    $("#timeLeft").css("display", "block");
+
+    $("#imageTrivia").attr("src", QUESTIONS[questionIndex].image);
+  }
+
+  // Function starts the game
+  // If questionIndex reach QUESTIONS length, game ends and score
+  // shows up, otherwise the games keeps running until last question
+  function startGame() {
+    if (questionIndex < QUESTIONS.length) {
+      correctAnswerElement = getTrivia();
+
+      checkCorrectAnswer();
+
+      intervalTimer();
+    } else {
+      questionIndex = 0;
+      $("#card").css("display", "none");
+      $("#correct").text("Correcto: " + correct);
+      $("#incorrect").text("Incorrecto: " + incorrect);
+      $("#score").css("display", "block");
+      var user = firebase.auth().currentUser;
+      auth.onAuthStateChanged((user) => {
+        var hoy = new Date();
+        var dd = hoy.getDate();
+        var mm = hoy.getMonth() + 1;
+        var yyyy = hoy.getFullYear();
+        var data_carpet = dd + "_" + mm + "_" + yyyy;
+
+        var firebaseref = firebase
+          .database()
+          .ref(
+            "Test/" +
+              document.getElementById("Titles").innerText +
+              "/" +
+              data_carpet +
+              "/" +
+              user.displayName
+          );
+        const fs = firebase.firestore();
+
+        firebaseref.push({
+          // fecha:dd+'/'+mm+'/'+yyyy,
+          usuario: user.displayName,
+          Hora:
+            hoy.getHours() + ":" + hoy.getMinutes() + ":" + hoy.getSeconds(),
+          Correctas: correct,
+          incorrectas: incorrect,
+        });
+      });
+    }
+  }
+
+  // Click event button starts game
+  $("#button").click(function () {
+    correct = 0;
+    incorrect = 0;
+    questionIndex = 0;
+    $("#card").css("display", "flex");
+    $("#button").css("display", "none");
+    startGame();
+  });
+
+  // Click event close score pop-up
+  $("#close").click(function () {
+    $("#score").css("display", "none");
+    $("#button").css("display", "inline-block");
+  });
+});
+
+("use strict");
+class Circle {
+  constructor(x, y, r, c, v, e) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.c = c;
+    this.v = v;
+    this.e = e;
+    this.a = 0.2;
+  }
+  draw() {
+    this.sn += 0.3;
+    this.y -= this.v;
+    this.y < this.e ? (this.a -= 0.02) : "";
+    san.beginPath();
+    san.arc(this.x, this.y, this.r, 0, 44 / 7, false);
+    san.fillStyle = `rgb(0,0,240)`;
+    san.fill();
+    san.closePath();
+  }
+  static push(n) {
+    var rad = Mnx(1, 5),
+      cx = Mnx(rad, W - rad),
+      cy = n ? Mnx(H + rad, H + rad + 100) : Mnx(H * 1.2, H * 0.3),
+      cl = Mnx(100, 250),
+      vl = Mnx(0.5, 2),
+      en = Mnx(H * 0.8, H * 0.25);
+    return new Circle(cx, cy, rad, cl, vl, en);
+  }
+}
+var Mnx = (n, x) => {
+    return Math.random() * (x - n) + n;
+  },
+  W = window.innerWidth,
+  H = window.innerHeight,
+  san;
+var ar = [];
+for (var i = 0; i < 550; i++) {
+  ar.push(Circle.push(false));
+}
+
+var draw = () => {
+  for (var i = 0; i < ar.length; i++) {
+    ar[i].draw();
+    if (ar[i].a < 0) {
+      ar[i] = Circle.push(true);
+    }
+  }
+};
+var init = () => {
+  var can = document.querySelector("canvas");
+  can.width = W;
+  can.height = H;
+  can.style.background = "white";
+  can.style.position = "fixed";
+  can.style.left = 0;
+  can.style.top = 0;
+  window.backdropFilter = "blur(2px)";
+  san = can.getContext("2d");
+  var up = () => {
+    //san.fillStyle=`rgba(0,0,0,.1)`
+    san.clearRect(0, 0, W, H);
+    draw();
+    webkitRequestAnimationFrame(up);
+  };
+  webkitRequestAnimationFrame(up);
+};
+onload = init;
